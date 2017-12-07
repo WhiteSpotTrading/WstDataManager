0f2b0c4c74e665eac2c0a9408562c1a5a3547b6b
carlwestman <carl.westman@gmail.com>
2017-12-07 17:41:26 +0100

    fixed requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
+from pip.req import parse_requirements
 
 setup(name='WstDataManager',
-      version='0.2',
+      version='0.3',
       description='Library of classes for maintaining Data used in WST-trading services',
       url='https://github.com/WhiteSpotTrading/WstDataManager',
       author='CarlWestman',
       author_email='carl.westman@gmail.com',
       license='None',
       packages=['WstDataManager', 'WstDataManager.morningstar'],
+      install_reqs = parse_requirements('requirements.txt', session='hack'),
       zip_safe=False)
